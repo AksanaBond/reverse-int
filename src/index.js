406cee6300b47bc632809aafc130bf2b9b9cfd82
Оксана Кухновец <oksana@MacBook-Air-Oksana.local>
2024-08-02 00:46:18 +0200

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let nString = n.toString();
+  let newNumber = "";
+  for (let i = nString.length - 1; i >= 0; i--){
+  newNumber = newNumber + nString.charAt(i);
+  }
+  return parseInt(newNumber);
 }
